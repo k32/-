1f6248ade7ef1a9e7b57fc28e90c67f5042f8db1
k32 <skmshj@gmail.com>
2016-04-12 21:06:19 +0000

    More or less stable checkpoint before large break

--- Voretion/Kobenation.hs
+++ Voretion/Kobenation.hs
@@ -16,35 +16,48 @@ data Kob = Kob
     , _down
     , _up   :: [Kob]
     } |
-    KL Link
-    deriving (Eq, Show, Read)             
+    TL Kob
+    deriving (Eq, Show, Read)
+
+{-             
+Example kobenation:
+ 
+  ______
+ /    __\  ___
+ |   /   \/   \  
+ A black cat runs.
+
+It's represented as:
+Kob "cat" [Kob "A" [] [], Kob "black" [] []] [Kob "runs"]
+
+It's seed-agnostic, for example:
+
+Kob "A" [] [Kob "cat" [Kob "black" [] []] [Kob "runs" [] []]]
+
+represents the same topology.
+-}
 
 data State = State {
       _threashold :: Float
     , _decay :: Float
     , _maxLinks :: Float
     }
-kobenate :: (MonadRandom m) => Ruleset -> String -> m String
-kobenate ruleset seed = undefined
 
--- Here we resolve ORs, multiconnectors and optional links
--- Rule's rval becomes flat, so we can represent it by two lists
--- It's based on the assumption that :&: is associative
-voretion :: (MonadRandom m, MonadReader State m) => Maybe LinkID -> Link -> m ([LinkID], [LinkID])
-voretion uplink link = v [] [] link
-    where
-        selectUp l = case uplink of
-                       Nothing -> l
-                       Just u  -> filter (containsID u) l
-      
-        v d u (Link l)
-          | _linkDirection l == Down = return (d ++ [l], u)
-          | True                     = return (d, u ++ [l])
-        v d u (LinkOr l) = do
-          let l' = selectUp l
-          n <- getRandomR (0, length l' - 1)
-          v d u (l' !! n)
-        v d u (MultiConnector l) = do
-          t <- asks _threashold
-          d <- asks _decay
-          
+-- Tvoretions (topological voretions)
+-- Here we resolve links
+tvoretion :: (MonadRandom m, MonadReader State m) => Ruleset -> Maybe LinkID -> Link -> m Kob
+tvoretion ruleset uplink = undefined  . kobenate uplink . resolveMacros ruleset
+
+-- Here we resolve ORs, multiconnectors and optionals
+-- Rule's rval becomes flat after that
+kobenate :: (MonadRandomm, MonadReader State m) => Link -> m ([LinkID], [LinkID])
+kobenate link =
+    case link of
+         Link l
+             | _linkDirection l == Down -> return ([l], [])
+             | True                     -> return ([], [l])
+         LinkOr l -> 
+             let l' = selectUp l
+             n <- getRandomR (0, length l' - 1)
+             kobenate (l' !! n)
+
