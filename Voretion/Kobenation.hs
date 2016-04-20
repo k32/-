b206d293de9f4b5d7dda2456d412bab4149c4267
k32 <skmshj@gmail.com>
2016-04-20 19:58:40 +0000

    tvoretions...

--- Voretion/Kobenation.hs
+++ Voretion/Kobenation.hs
@@ -9,14 +9,13 @@ import Control.Monad.Random
 import Control.Monad.Reader
 import LinkGrammar.AST
 import LinkGrammar.Process
-import Control.Arrow    
+import Control.Arrow
 
 -- Kobenation    
 data Kob = Kob
     {
-      _text :: String
-    , _down
-    , _up   :: [Kob]
+      _Τ :: String
+    , _Æ :: ([Kob], [Kob])
     } |
     TL String Link
     deriving (Eq, Show)
@@ -41,24 +40,31 @@ represents the same topology.
 
 data State = State {
       _threashold :: Float
-    , _decay :: Float
-    , _maxLinks :: Float
+    , _decayₒ
+    , _decayₘ :: Float
     }
 
 -- Tvoretions (topological voretions)
--- Here we resolve links
+-- Here we kobenate and resolve links
 tvoretion :: (MonadRandom m, MonadReader State m) => Ruleset -> Kob -> m Kob
-tvoretion ruleset = undefined
+tvoretion ruleset (TL τ ł) = do
+    (δ₀, υ₀) <- kobenate [] ł -- TODO: not enough psihoza... make it pointfree
+    δ₁ <- mapM (f Minus) δ₀
+    υ₁ <- mapM (f Plus) υ₀
+    return $ Kob τ (δ₁, υ₁)
+  where
+    f :: (MonadRandom m, MonadReader State m) => LinkDirection -> LinkName -> m Kob
+    f ϧ
 
 -- Here we resolve ORs, multiconnectors and optionals
--- Rule's rval becomes flat after that
+-- Rule's rval becomes flat after that, only &s and linkIDs stand
 kobenate :: (MonadRandom m, MonadReader State m) => [Int] -> Link -> m ([LinkName], [LinkName])
 kobenate idx (Node t c) =
     case t of
       Optional ->
           case idx of
             (_:idx') ->
-                vorecOpt $ kobenate idx' $ head c
+                vorecOpt ([], []) $ kobenate idx' $ head c
             [] ->
                 kobenate [] $ head c
       LinkAnd -> do
@@ -67,24 +73,41 @@ kobenate idx (Node t c) =
                                   | φ == α -> (idx', x)
                               _ ->
                                   ([], x)
-                 l' = map ρ (zip c [0..]) 
+                 l' = map ρ (zip c [0..])
              (concat *** concat . reverse) <$> unzip <$> mapM (uncurry kobenate) l'
-      LinkOr -> do
-          let n = length c
-          i <- getRandomR (0,n-1)
-          kobenate [] $ c !! i
+      LinkOr ->
+          case idx of
+            [] -> do
+              let n = length c
+              i <- getRandomR (0,n-1)
+              kobenate [] $ c !! i
+            (α:idx') ->
+              kobenate idx' $ c !! α
       Link (LinkID n d) ->
           case d of
             Plus -> return ([], [n])
             Minus -> return ([n], [])
       MultiConnector ->
-          -- TODO: implement me
-          --(concat *** concat . reverse) <$> unzip <$> (vorecMul $ kobenate [] $ head c)
-          return ([], [])
-          
-vorecOpt :: (MonadRandom m, MonadReader State m) => m a -> m a
-vorecOpt = undefined
+          (concat *** concat . reverse) <$> unzip <$> (vorecMul $ kobenate (drop 1 idx) $ head c)
+      Cost _ -> 
+          kobenate (drop 1 idx) $ head c
+
+vorecOpt :: (MonadRandom m, MonadReader State m) => a -> m a -> m a
+vorecOpt d a = do
+  x <- getRandom
+  t <- asks _threashold
+  if x < t
+    then local (\s@State{_threashold=t₀, _decayₒ=δ}->s{_threashold=t₀*δ}) a
+    else return d
+      
 
 vorecMul :: (MonadRandom m, MonadReader State m) => m a -> m [a]
-vorecMul = undefined
+vorecMul a = do
+  x <- getRandom
+  t <- asks _threashold
+  if x < t
+    then local (\s@State{_threashold=t₀, _decayₘ=δₘ}->s{_threashold=t₀*δₘ}) $ 
+               (:) <$> a <*> vorecMul a
+    else return []
+
            
